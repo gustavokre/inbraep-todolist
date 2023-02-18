2e8a786aba527462257c77f4794ab3b747e004cb
Gustavo Kreibich <gukre92@gmail.com>
2023-02-18 18:37:27 -0300

    Metodos adicionados para todas as rotas

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pessoas;
 use App\Models\Tarefas;
+use App\Models\TarefasMensagem;
 use App\Models\TarefasPrioridade;
 use App\Models\TarefasProgresso;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class TarefasController extends Controller
 {
+    public function salvarMensagem(Request $request){
+
+        try {
+
+            $mensagem = new TarefasMensagem($request->all());
+            $mensagem->save();
+
+            return response()->json(["message" => ["Mensagem salva com sucesso"]], 200);
+
+        } catch (\Throwable $e) {
+            log::error($e->getMessage());
+            return response()->json(["errors" => [$e->getMessage()]], 500);
+        }
+
+    }
+
     public function getTarefasProgresso(Request $request){
 
         return response()->json(TarefasProgresso::all(), 200);
@@ -25,12 +45,85 @@ class TarefasController extends Controller
 
     }
 
-    public function getTarefas(Request $request){
+    public function getTarefas(Request $request) {
 
-        //groupby cd_tarefa_progresso para facilitar o front-end
-        $tarefas = Tarefas::all()->groupBy('cd_tarefa_progresso');
+
+        $tarefas = Tarefas::where('fl_delete', false)
+            ->with('prioridade')
+            ->get()
+            //ordena as tarefas por prioridade
+            ->sortBy(function($query){
+
+                return $query->prioridade->or_prioridade;
+
+            })
+            //groupby cd_tarefa_progresso para facilitar o front-end
+            ->groupBy('cd_tarefa_progresso');
 
         return response()->json($tarefas, 200);
 
+    }
+
+    public function getPessoas(Request $request){
+
+        return response()->json(Pessoas::all(), 200);
+    }
+
+    public function getMensagem(Request $request, $id){
+
+        $tarefa_mensagem = Tarefas::with('mensagens')->find($id);
+
+        return response()->json($tarefa_mensagem, 200);
+    }
+
+    public function store(Request $request){
+        try {
+
+            DB::beginTransaction();
+
+            $tarefa = new Tarefas($request->all());
+            $tarefa->save();
+
+            $mensagem = new TarefasMensagem();
+            $mensagem->cd_pessoa = $request->cd_pessoa_requerente;
+            $mensagem->ds_mensagem = $request->ds_mensagem;
+            $mensagem->cd_tarefa = $tarefa->id;
+            $mensagem->save();
+
+            DB::commit();
+
+        } catch (\Throwable $e) {
+
+            log::error($e->getMessage());
+            DB::rollBack();
+            return response()->json(["errors" => ["Não foi possível excluir esta tarefa"]], 500);
+        }
+    }
+
+    public function update(Request $request, Tarefas $tarefa){
+        try {
+
+            $tarefa->update($request->all());
+
+        } catch (\Throwable $e) {
+
+            log::error($e->getMessage());
+            return response()->json(["errors" => ["Não foi possível excluir esta tarefa"]], 500);
+        }
+    }
+
+    public function destroy(Tarefas $tarefa){
+        try {
+
+            $tarefa->fl_delete = true;
+            $tarefa->save();
+
+            return response()->json(["message" => ["Tarefa removida"]], 200);
+
+        } catch (\Throwable $e) {
+
+            log::error($e->getMessage());
+            return response()->json(["errors" => ["Não foi possível excluir esta tarefa"]], 500);
+        }
     }
 }
