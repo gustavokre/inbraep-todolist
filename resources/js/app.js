eb7a69c7d537110bc66f4cf1df58d2cd17e2c645
Gustavo Kreibich <gukre92@gmail.com>
2023-02-18 21:27:10 -0300

    Alteracao da linguagem para portugues (dependecia)

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,11 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 import "devextreme/dist/css/dx.light.css";
+import ptMessages from "devextreme/localization/messages/pt.json";
+import { locale, loadMessages } from "devextreme/localization";
+
+loadMessages(ptMessages);
+locale(navigator.language);
 
 import { createApp } from 'vue';
 
@@ -11,8 +16,11 @@ import app from './Components/App.vue';
 import router from './router'
 import store from './store'
 
-createApp(app)
+const vueApp = createApp(app)
     .use(router)
     .use(store)
     .use(VueSweetalert2)
     .mount('#app')
+
+
+
